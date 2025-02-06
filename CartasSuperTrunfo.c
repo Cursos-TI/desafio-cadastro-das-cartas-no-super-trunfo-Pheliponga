33dce9bd7d959d09a528aec69bee161aab121fda
Pheliponga <lipperovha@gmail.com>
2025-02-06 20:09:48 -0300

    COMEÇO DOS PROGRAMAS.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,3 +1,58 @@
-#inclue <stdio.h>
+#include <stdio.h>
 
-int main
+int main(){ 
+  int populacao2_1_1, populacao2_1_2, populacao2_1_3, populacao2_1_4; 
+  int populacao2_2_1, populacao2_2_2, populacao2_2_3, populacao2_2_4;
+  int populacao2_3_1, populacao2_3_2, populacao2_3_3, populacao2_3_4;
+  int populacao2_4_1, populacao2_4_2, populacao2_4_3, populacao2_4_4;
+  int populacao2_5_1, populacao2_5_2, populacao2_5_3, populacao2_5_4;
+  int populacao2_6_1, populacao2_6_2, populacao2_6_3, populacao2_6_4;
+  int populacao2_7_1, populacao2_7_2, populacao2_7_3, populacao2_7_4;
+  int populacao2_8_1, populacao2_8_2, populacao2_8_3, populacao2_8_4;
+
+  int turistico2_1_1, turistico2_1_2, turistico2_1_3, turistico2_1_4; 
+  int turistico2_2_1, turistico2_2_2, turistico2_2_3, turistico2_2_4;
+  int turistico2_3_1, turistico2_3_2, turistico2_3_3, turistico2_3_4;
+  int turistico2_4_1, turistico2_4_2, turistico2_4_3, turistico2_4_4;
+  int turistico2_5_1, turistico2_5_2, turistico2_5_3, turistico2_5_4;
+  int turistico2_6_1, turistico2_6_2, turistico2_6_3, turistico2_6_4;
+  int turistico2_7_1, turistico2_7_2, turistico2_7_3, turistico2_7_4;
+  int turistico2_8_1, turistico2_8_2, turistico2_8_3, turistico2_8_4;
+
+  float pib2_1_1, pib2_1_2, pib2_1_3, pib2_1_4; 
+  float pib2_2_1, pib2_2_2, pib2_2_3, pib2_2_4;
+  float pib2_3_1, pib2_3_2, pib2_3_3, pib2_3_4;
+  float pib2_4_1, pib2_4_2, pib2_4_3, pib2_4_4;
+  float pib2_5_1, pib2_5_2, pib2_5_3, pib2_5_4;
+  float pib2_6_1, pib2_6_2, pib2_6_3, pib2_6_4;
+  float pib2_7_1, pib2_7_2, pib2_7_3, pib2_7_4;
+  float pib2_8_1, pib2_8_2, pib2_8_3, pib2_8_4;
+
+  float area2_1_1, area2_1_2, area2_1_3, area2_1_4; 
+  float area2_2_1, area2_2_2, area2_2_3, area2_2_4;
+  float area2_3_1, area2_3_2, area2_3_3, area2_3_4;
+  float area2_4_1, area2_4_2, area2_4_3, area2_4_4;
+  float area2_5_1, area2_5_2, area2_5_3, area2_5_4;
+  float area2_6_1, area2_6_2, area2_6_3, area2_6_4;
+  float area2_7_1, area2_7_2, area2_7_3, area2_7_4;
+  float area2_8_1, area2_8_2, area2_8_3, area2_8_4;
+
+  char nome1_1[50], nome1_2[50], nome1_3[50];
+  
+  printf("\nBEM VINDO AO SUPER TRUNFO!\n");
+  printf("\nHORA DE COMEÇAR A JOGAR!\n");
+
+  printf("\nDIGITE O NOMDE DO PAÍS DESEJADO:");
+  scanf("%s", &nome1_1);
+  printf("\nSEU PAÍS: %s\n", &nome1_1);
+
+    }
+
+
+
+
+  
+
+
+
+}
