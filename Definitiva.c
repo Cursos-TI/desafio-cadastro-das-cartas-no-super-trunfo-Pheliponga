5f6f143b4d9f7a3b88055f362bcfeec89d2cc80c
Pheliponga <lipperovha@gmail.com>
2025-02-26 23:47:31 +0000

    Definitiva

---
+++ Definitiva.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int populacaoo1_1, população2_1;
+    int turistico1_1, turistico2_1;
+    char estado1_1, estado2_1;
+    char cidade1_1, cidade2_1;
+    char pais;
+    float pib1_1, pib2_1;
+    float area1_1, area2_1;
+
+    printf("\nBem-vindo ao Super Trunfo!\n");
+
+    printf("\nEscolha o seu País para começar:\n");
+    scanf("%s", pais);
+
+
+
+}
