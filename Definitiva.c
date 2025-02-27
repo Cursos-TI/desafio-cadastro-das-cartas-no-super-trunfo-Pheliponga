5dccedbe0655890d4d1041e24e05ebcf8bdfeaac
Pheliponga <lipperovha@gmail.com>
2025-02-27 01:55:41 +0000

    Continuação

--- Definitiva.c
+++ Definitiva.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
 int main(){
-
-    int populacaoo1_1, população2_1;
+    int populacao1_1, populacao2_1;
     int turistico1_1, turistico2_1;
+    int carta1=1, carta2=2;
     char estado1_1, estado2_1;
     char cidade1_1, cidade2_1;
-    char pais;
+    char pais[20];
     float pib1_1, pib2_1;
     float area1_1, area2_1;
 
     printf("\nBem-vindo ao Super Trunfo!\n");
+    printf("\nHora de começar a jogar!\n");
 
     printf("\nEscolha o seu País para começar:\n");
-    scanf("%s", pais);
+    scanf("%s", &pais);
+    printf("\nPaís escolhido: %s\n", &pais);
 
+    printf("\nCarta: %d", &carta1);
 
+    printf("\nEscolha o seu ESTADO A01")
+
+    return 0; 
 
 }
