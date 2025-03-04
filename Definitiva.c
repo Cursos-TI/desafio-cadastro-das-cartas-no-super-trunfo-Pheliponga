9bd9aa26e72b43d62d300e401c5bb18fa6f3deb3
Pheliponga <lipperovha@gmail.com>
2025-03-04 19:50:50 +0000

    Desafio um concluido

--- Definitiva.c
+++ Definitiva.c
@@ -4,9 +4,9 @@ int main(){
     int populacao1_1, populacao2_1;
     int turistico1_1, turistico2_1;
     int carta1 = 1, carta2 = 2;
-    char estado1_1[20], estado2_1[16];
-    char cidade1_1[15], cidade2_1[17];
-    char pais[19];
+    char estado1_1[20], estado2_1[20];
+    char cidade1_1[20], cidade2_1[20];
+    char pais[20];
     float pib1_1, pib2_1;
     float area1_1, area2_1;
    
@@ -17,6 +17,8 @@ int main(){
     printf("\nEscolha o seu País para começar: ");
     scanf(" %s", &pais);
     printf("\nPaís escolhido: %s\n", pais);
+
+    // CARTA 1 COMEÇA AQUI!
 
     printf("\nCarta: %d\n", carta1);
 
@@ -41,10 +43,40 @@ int main(){
     printf("\n- PONTOS TURÍSTICOS: ");
     scanf("%d", &turistico1_1);
 
-//agora vou exibir os dados//
+    // CARTA 2 COMEÇA AQUI!
 
-    printf("\nCarta: %d\n" "País: %s\n" "Estado: %s\n" "Código: A01\n" "Cidade: %s\n" "População: %d\n" "Area: %f\n"
-        "PIB: %f\n" "Pontos Turísticos: %d\n", carta1, pais, estado1_1, cidade1_1, populacao1_1, area1_1, pib1_1, turistico1_1);
+    printf("\nCarta: %d\n", carta2);
+
+    printf("\nEscolha o seu ESTADO (B): ");
+    scanf("%s", &estado2_1);
+    printf("\nSeu estado: %s\n", estado2_1);
+
+    printf("\nEscolha a sua CIDADE preferida: ");
+    scanf("%s", &cidade2_1);
+
+    printf("\nInforme os seguintes dados da CIDADE (B02): POPULAÇÃO, ÁREA, PIB e o NÚMERO DE PONTOS TURÍSTICOS!\n");
+
+    printf("\n- POPULAÇÃO: ");
+    scanf("%d", &populacao2_1);
+
+    printf("\n- ÁREA: ");
+    scanf("%f", &area2_1);
+
+    printf("\n- PIB: ");
+    scanf("%f", &pib2_1);
+
+    printf("\n- PONTOS TURÍSTICOS: ");
+    scanf("%d", &turistico2_1);
+
+    // EXIBIÇÃO DOS DADOS DAS CARTAS!
+
+    printf("\nEstas são suas cartas:\n");
+    
+    printf("\nCarta: %d\n" "Código: A01\n" "País: %s\n" "Estado: %s\n" "Cidade: %s\n" "População: %d\n" "Área: %.3f km²\n"
+        "PIB: %.2f bilhões de reais\n" "Pontos Turísticos: %d\n", carta1, pais, estado1_1, cidade1_1, populacao1_1, area1_1, pib1_1, turistico1_1);
+    
+    printf("\nCarta: %d\n" "Código: B02\n" "País: %s\n" "Estado: %s\n" "Cidade: %s\n" "População: %d\n" "Área: %.3f km²\n"
+        "PIB: %.2f bilhões de reais\n" "Pontos Turísticos: %d\n", carta2, pais, estado2_1, cidade2_1, populacao2_1, area2_1, pib2_1, turistico2_1);    
     
     return 0; 
 
